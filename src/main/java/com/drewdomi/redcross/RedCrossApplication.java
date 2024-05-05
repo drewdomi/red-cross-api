8d962732342f2750a366250ec49fb2ebde4eaf41
Andrew Domingues <drewdomi@proton.me>
2024-05-05 14:12:58 -0300

    feat: add docker with postgres

--- src/main/java/com/drewdomi/redcross/RedCrossApplication.java
+++ src/main/java/com/drewdomi/redcross/RedCrossApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RedCrossApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedCrossApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RedCrossApplication.class, args);
+  }
 
 }
