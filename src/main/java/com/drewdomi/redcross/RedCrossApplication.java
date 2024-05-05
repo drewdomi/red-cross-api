d8363a2b94bf88851ea7f9231d099daf3a40b261
Andrew Domingues <drewdomi@proton.me>
2024-05-05 13:59:35 -0300

    chore: create template springboot

---
+++ src/main/java/com/drewdomi/redcross/RedCrossApplication.java
@@ -0,0 +1,13 @@
+package com.drewdomi.redcross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedCrossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedCrossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drewdomi/redcross/RedCrossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drewdomi.redcross;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedCrossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
