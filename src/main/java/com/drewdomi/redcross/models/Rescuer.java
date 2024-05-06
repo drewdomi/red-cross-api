630bcc5fd75699c7004785773b4eb43398d055e3
Andrew Domingues <drewdomi@proton.me>
2024-05-06 10:44:27 -0300

    feat: add columns to rescuer model

--- src/main/java/com/drewdomi/redcross/models/Rescuer.java
+++ src/main/java/com/drewdomi/redcross/models/Rescuer.java
@@ -1,21 +1,30 @@
 package com.drewdomi.redcross.models;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rescuer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Email
@@ -29,15 +38,21 @@ public class Rescuer {
     @Size(min = 8, message = "Password must have at least 8 characters")
     private String password;
 
-    public Rescuer() {
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
     }
 
-    public Rescuer(@Email String email,
-            @Size(min = 2, message = "Name must have at least 3 characters") @Pattern(regexp = "^[A-Z]+(.)*") @NotBlank(message = "Name mandatory") String name,
-            @Size(min = 8, message = "Password must have at least 8 characters") String password) {
-        this.email = email;
-        this.name = name;
-        this.password = password;
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
     }
 
 }
