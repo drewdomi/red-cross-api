7b7481c99d12dc2873c73e9bba6ebe580ccf30eb
Andrew Domingues <drewdomi@proton.me>
2024-05-09 13:04:58 -0300

    feat: initial security config

---
+++ src/main/java/com/drewdomi/redcross/configs/security/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.drewdomi.redcross.configs.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/**")
+                            .permitAll();
+                });
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var normalUser = User.builder()
+                .username("drew").password("1234").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(normalUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
