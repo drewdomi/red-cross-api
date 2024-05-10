dc93c34379ac62ac7487f3ce8df2e75ecb3a1732
Andrew Domingues <drewdomi@proton.me>
2024-05-10 20:42:45 -0300

    refactor: add validation to errors 400

--- src/main/java/com/drewdomi/redcross/infra/errors/ErrorHandler.java
+++ src/main/java/com/drewdomi/redcross/infra/errors/ErrorHandler.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import jakarta.validation.UnexpectedTypeException;
+
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,39 +18,38 @@ import org.springframework.web.context.request.WebRequest;
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler(value = { NoResourceFoundException.class, HttpRequestMethodNotSupportedException.class })
+    @ExceptionHandler(value = {
+            NoResourceFoundException.class,
+            HttpRequestMethodNotSupportedException.class
+    })
     public ResponseEntity<Object> error404() {
-        String errorMessage = "Not found";
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(Map.of("message", errorMessage));
+                .body(Map.of("message", "Not found"));
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler(value = {
+            DataIntegrityViolationException.class,
+            UnexpectedTypeException.class,
+            MethodArgumentNotValidException.class
+    })
     public ResponseEntity<Object> error400(
-            MethodArgumentNotValidException ex) {
+            RuntimeException ex) {
 
-        String errorMessage = ex.getFieldError().getDefaultMessage();
+        final String errorMessage = ex.getMessage();
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(Map.of("message", errorMessage));
     }
 
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<Object> handleDataIntegrityViolationException(
-            DataIntegrityViolationException ex) {
-
-        String errorMessage = ex.getMessage();
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of("message", errorMessage));
-    }
-
-    @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
+    @ExceptionHandler(value = {
+            IllegalArgumentException.class,
+            IllegalStateException.class
+    })
     protected ResponseEntity<Object> error409(
             RuntimeException ex, WebRequest request) {
 
-        String errorMessage = ex.getMessage();
+        final String errorMessage = ex.getMessage();
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .body(Map.of("message", errorMessage));
