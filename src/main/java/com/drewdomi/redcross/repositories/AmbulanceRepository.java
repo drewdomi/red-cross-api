82bf64f297cb54478e0a919f14bc5d0135309ffb
Andrew Domingues <contato@drewdomi.com>
2024-06-16 11:30:08 -0300

    feat: create ambulance model

---
+++ src/main/java/com/drewdomi/redcross/repositories/AmbulanceRepository.java
@@ -0,0 +1,11 @@
+package com.drewdomi.redcross.repositories;
+
+import com.drewdomi.redcross.models.Ambulance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AmbulanceRepository extends JpaRepository<Ambulance, UUID> {
+}
